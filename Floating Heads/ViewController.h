ee4f5c25b2d64e7fb505053a521e47f9664be04d
manuntag <manuntag@ualberta.ca>
2015-02-18 12:39:14 -0800

    Initial Commit

---
+++ Floating Heads/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Floating Heads
+//
+//  Created by David Manuntag on 2015-02-18.
+//  Copyright (c) 2015 David Manuntag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Floating Heads/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Floating Heads
+//
+//  Created by David Manuntag on 2015-02-18.
+//  Copyright (c) 2015 David Manuntag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
